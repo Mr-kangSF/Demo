2b5955f7971b2389ec7052ea786b7663f60d3cd0
wankang <1926184697@qq.com>
2019-07-29 17:59:34 +0800

    两个默认方法都同样具体时，你需要在类中覆盖该方法，显式地选择使用哪个接口中提供的默认方法。

--- demo/src/main/java/com/wankang/demo/service/java8/DefaultMethodTest02.java
+++ demo/src/main/java/com/wankang/demo/service/java8/DefaultMethodTest02.java
@@ -1,17 +1,16 @@
 package com.wankang.demo.service.java8;
 
 /**
- *
+ * default 方法显式声明调用
+ * 两个默认方法都同样具体时，你需要在类中覆盖该方法，显式地选择使用哪个接口中提供的默认方法。
  *
  * @author wankang
- * @create 2019-07-25 16:42
+ * @create 2019-07-25 16:49
  **/
-public class DefaultMethodTest {
+public class DefaultMethodTest02 {
 
     public static void main(String... args) {
-//        new C().hello();
-//        new E().hello();
-        new G().hello();
+        new C().hello();
     }
 
     static interface A {
@@ -20,27 +19,16 @@ public class DefaultMethodTest {
         }
     }
 
-    static interface B extends A {
+    static interface B {
         public default void hello() {
             System.out.println("Hello from B");
         }
     }
 
     static class C implements B, A {
-    }
-
-    static class D implements A {
-    }
-
-    static class E extends D implements B, A {
-    }
-
-    static class F implements B, A {
         public void hello() {
-            System.out.println("Hello from F");
+            A.super.hello();  // 必须显示声明调用哪个方法
         }
     }
 
-    static class G extends F implements B, A {
-    }
 }
